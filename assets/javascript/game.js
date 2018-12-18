2b414efdbb0027e3460605889c2d0b61ef962c4a
Susan Uttro <souttro@gmail.com>
2018-12-18 15:42:12 -0500

    initial push

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+
+// Variables 
+var  score;
+var crysOne;
+var crysTwo;
+var crysThree;
+var crysFour ;
+var scoreCounter; 
+var wins;
+var losses ;
+var randomNum = Math.floor(Math.random() * 120);
+
+//computer creates random number.
+// ;
+    // The random number is shown at the start of the game should be between 19 - 120.
+ 
+// Ramdom number remains on the screen untill user matches number.
+
+// The player will be shown a random number at the start of the game.
+
+// The user is required to match the random number to win the game.
+var randomNum;
+$("number-to-click").text(randomNum);
+var counter = 0;
+$(".crystal-image").on("click", function() {
+  alert( "I clicked a crystal");
+
+})
+// Each crystal should have a random hidden value between 1 - 12.
+// game has four crystal buttons with randoms numbers embedded into them.  
+
+// when each crystal button is pushed the the numbers is shown.
+alert("user new score is: " + counter);
+
+// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+counter += 10;
+// when user matches ramdon number then user wins and random number resets.
+// Also, all the crystals will have four new hidden values. 
+    // The user's score and score counter will reset to zero.
+
+    // When the game begins again, the player should see a new random number.  
+   // each time user wins the score goes up by one.
+//when user does not watch the ramdon number a loss is recorded
+//when user looses computer  wins and score goes up by one.
+ 
+
+   
+    
+
+
+
+
+
+
